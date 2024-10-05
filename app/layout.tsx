54c6ccfd9c48b957a50c883e5af14a46132bff38
codinister <codinister@gmail.com>
2024-10-05 19:25:16 +0100

    Commit 22

--- app/layout.tsx
+++ app/layout.tsx
@@ -27,21 +27,32 @@ export default function RootLayout({
         <QueryClientProvider client={provider}>
           <Suspense>{children}</Suspense>
           <Footer />
-          
+
+
+
+
+  
+  
           <Script id="1253179435823295" strategy="afterInteractive">
-            {`
-          !function(f,b,e,v,n,t,s)
-          {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-          n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-          if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-          n.queue=[];t=b.createElement(e);t.async=!0;
-          t.src=v;s=b.getElementsByTagName(e)[0];
-          s.parentNode.insertBefore(t,s)}(window, document,'script',
-          'https://connect.facebook.net/en_US/fbevents.js');
-          fbq('init', 'YOUR_PIXEL_ID');
-          fbq('track', 'PageView');
-        `}
-          </Script>
+  {`
+ !function(f,b,e,v,n,t,s)
+  {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+  n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+  if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+  n.queue=[];t=b.createElement(e);t.async=!0;
+  t.src=v;s=b.getElementsByTagName(e)[0];
+  s.parentNode.insertBefore(t,s)}(window, document,'script',
+  'https://connect.facebook.net/en_US/fbevents.js');
+  fbq('init', '{1253179435823295}');
+  fbq('track', 'PageView');
+`}
+
+</Script>
+<noscript>
+  <Image alt="" height="1" width="1" 
+       src="https://www.facebook.com/tr?id={1253179435823295}&ev=PageView&noscript=1"/>
+</noscript>
+
         </QueryClientProvider>
       </body>
     </html>
