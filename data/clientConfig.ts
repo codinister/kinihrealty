e8f8d1c109602c62d9adb30f099504c4d7323ef4
codinister <codinister@gmail.com>
2024-11-20 12:11:51 +0100

    Commit 59

--- data/clientConfig.ts
+++ data/clientConfig.ts
@@ -5,7 +5,7 @@ const client = createClient({
   dataset: 'production',
   apiVersion: '2024-08-13',
   useCdn: true,
-  token:'skl6ENpbCgOzS3MLgPEgfyVTjjynHfhMznYaLRhjswcrh8qUYvPBW8blFBT1NtU9bOv9wm5lKwYouiNCaElGSUD2ttWMyuak0oUQ0XeEz5b3CIlCXdwp1ranKAmBfVgrFEzwLUDtyRX5jszOtkAG3yHmytqVgXOn7CbEg0E7AZXL5YC2Pvly',
+  token: `skl6ENpbCgOzS3MLgPEgfyVTjjynHfhMznYaLRhjswcrh8qUYvPBW8blFBT1NtU9bOv9wm5lKwYouiNCaElGSUD2ttWMyuak0oUQ0XeEz5b3CIlCXdwp1ranKAmBfVgrFEzwLUDtyRX5jszOtkAG3yHmytqVgXOn7CbEg0E7AZXL5YC2Pvly`,
 });
 
 export default client;
